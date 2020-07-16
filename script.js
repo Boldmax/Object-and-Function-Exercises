b001cdeea730a8fd21993d4898fe4a754fed6617
Boldmax <maxbold0007@gmail.com>
2020-07-16 05:24:18 +0100

    exercise_1

---
+++ script.js
@@ -0,0 +1,22 @@
+// Exercise 1
+var aboutMe = {
+    firstName: "Olabode",
+    lastName: "Adewumi",
+    Occupation: "Software Developer"
+    }
+
+    // Exercise 2
+    aboutMe.firstName //"Olabode"
+    aboutMe.lastName //"Adewumi"
+    aboutMe.Occupation //"Software Developer"
+    aboutMe['firstName'] //"Olabode"
+    aboutMe['lastName'] //"Adewumi"
+    aboutMe["Occupation"] //"Software Developer"
+
+    // Exercise 3
+    aboutMe.hobby = 'coding' //"coding"
+
+    delete aboutMe.Occupation // true
+
+    // Exercise 4
+    
